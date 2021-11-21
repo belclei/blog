bd4eefd16d2d4c6183bb75184670f073f876a395
belclei <belclei@gmail.com>
2021-11-20 22:13:33 -0300

    add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <h1>oi</h1>
+  return (
+    <>
+      <h1>oi</h1>
+      <p>Teste</p>
+    </>
+  )
 }
 
 export default Home
