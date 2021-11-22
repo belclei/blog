43481089795b195c6ff75a152da92dab8c571793
belclei <belclei@gmail.com>
2021-11-21 21:39:30 -0300

    fix possible null reference

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -8,10 +8,13 @@ export default function Comments(): JSX.Element {
     script.setAttribute('src', 'https://utteranc.es/client.js')
     script.setAttribute('crossorigin', 'anonymous')
     script.setAttribute('async', 'true')
-    script.setAttribute('repo', 'belclei/belclei-dev')
+    script.setAttribute('repo', 'belclei/blog')
     script.setAttribute('issue-term', 'pathname')
     script.setAttribute('theme', 'dark-blue')
-    anchor!.appendChild(script)
+
+    if (anchor) {
+      anchor.appendChild(script)
+    }
   }, [])
 
   return <div id="comments" />
