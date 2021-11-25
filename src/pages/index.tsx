aced4af1a2280cd185954317abcf05c86be09e68
belclei <belclei@gmail.com>
2021-11-24 21:55:37 -0300

    feed index components with md files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,45 +7,56 @@ import { getAllPosts } from '../services/Content'
 import { metaProps } from '../utils/types'
 
 interface BlogProps {
-  title: string
+  posts: [
+    {
+      slug: string
+      title: string
+      subtitle: string
+      createdAt: string
+      createdAt_formatted: string
+      timeToRead: number
+      image: string
+      content: string
+    }
+  ]
 }
 const Blog: NextPage<BlogProps> = (props: BlogProps) => {
   console.log('🚀 ~ file: index.tsx ~ line 10 ~ props', props)
   const meta: metaProps = {
-    title: 'titulo',
-    canonical: 'https://www.belclei.dev.br/eita',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime assumenda perferendis veniam culpa beatae distinctio, aliquam vero inventore quos, iusto pariatur cum labore impedit veritatis natus id doloremque, similique placeat.',
-    url: 'https://www.belclei.dev.br',
-    post: {
-      date: '2020-12-03',
-      image: '/posts/000/helloworld.jpg',
-      modified_date: '2020-12-05'
-    }
+    title: 'Blog'
   }
+
   return (
     <Main meta={meta}>
-      <PostCard />
-      <PostCard />
-      <PostCard />
+      {props.posts.map(post => (
+        <PostCard
+          key={post.slug}
+          link={`/blog/${post.slug}`}
+          title={post.title}
+          subtitle={post.subtitle}
+          createdAt={post.createdAt_formatted}
+          image={post.image}
+          timeToRead={post.timeToRead}
+        />
+      ))}
     </Main>
   )
 }
 
-export const getStaticProps: GetStaticProps = async context => {
+export const getStaticProps: GetStaticProps = async () => {
   const posts = getAllPosts()
   const formattedPosts = posts.map(post => {
     const timeToRead = Math.ceil(post.content.split(' ').length / 200)
     const createdAt_formatted = formatRelative(new Date(post.createdAt), new Date(), {
       locale: ptBR
     })
-    return { timeToRead, createdAt_formatted }
+    return { timeToRead, createdAt_formatted, ...post }
   })
   console.log('🚀 ~ file: index.tsx ~ line 44 ~ formattedPosts', formattedPosts)
 
   return {
     props: {
-      title: 'éita'
+      posts: formattedPosts
     },
     revalidate: 60 * 60 * 24 // 1 dia
   }
