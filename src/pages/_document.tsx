a6a05738e15206657ee9e0235d253eff7c26fc98
belclei <belclei@gmail.com>
2021-12-08 22:03:23 -0300

    colors and fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Kaushan+Script&family=Mukta:wght@500&family=Montserrat:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Kaushan+Script&family=Aleo:wght@700&family=Quicksand:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap"
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="/favicon/favicon.png" type="image/png" />
