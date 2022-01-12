be92c5f8930e4a13771f78e38e6ab8bca8f0e585
belclei <belclei@gmail.com>
2022-01-12 20:44:49 -0300

    meta files

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -12,17 +12,33 @@ const Meta = (props: metaProps) => {
     <Head>
       <meta charSet="UTF-8" key="charset" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" key="apple" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" key="icon32" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" key="icon16" />
+      <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
+
+      <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+
       <link rel="icon" href="/favicon/favicon.ico" key="favicon" />
+      <link rel="manifest" href="/favicon/manifest.json" />
+
       <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
       <meta name="MobileOptimized" content="320" />
       <meta name="HandheldFriendly" content="True" />
-      <meta name="theme-color" content="#220054" />
+      <meta name="theme-color" content="#fd7014" />
       <meta name="referrer" content="no-referrer-when-downgrade" />
-      <meta name="msapplication-TileColor" content="#220054" />
+      <meta name="msapplication-TileColor" content="#fd7014" />
+      <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png"></meta>
       <meta name="google" content="notranslate" />
+
       <title>{`${props.title} | ${Config.site_name}`}</title>
       <meta name="description" content={props.description ?? Config.description} key="description" />
       <meta name="author" content={Config.author} key="author" />
